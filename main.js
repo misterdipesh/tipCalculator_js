032393f7f98164ce5eef469ab33c58abafb0644c
Dipesh Bishwakarma <deepeesh471@gmail.com>
2020-12-04 13:32:49 +0545

    final_version

--- main.js
+++ main.js
@@ -3,6 +3,14 @@ let bill=parseInt(document.getElementById('billAmount').value);
 let people=parseInt(document.getElementById('peopleSize').value);
 let serviceSatisfaction=parseInt(document.getElementById('service').value);
 let result=bill/people*serviceSatisfaction/100;
-console.log(result);
-document.getElementById('calculatedResult').innerText=result;
+if (isNaN(result)){
+document.getElementById('calculatedResult').innerHTML=
+`<p style="color:red;">Please fill the corresponding vlaues</p>`;
+}
+else{
+document.getElementById('calculatedResult').innerHTML=
+`<p>${result} is the tip </p>`;
+}
+
+
 }
